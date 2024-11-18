60e602b1ca63f0a918d870020b83d08ba61a0c21
plus <ctre@users.sourceforge.net>
2024-11-18 10:59:11 +0200

    fix: Select combobox flashing when switching rules

--- ui/select.cpp
+++ ui/select.cpp
@@ -1,10 +1,41 @@
 #include "ui/select.h"
 #include "commctrl.h" // WC_COMBOBOX
-#include <uxtheme.h> // SetWindowTheme
 #include <windowsx.h> // ComboBox macros
 #include "utility/string_conversion.h" // string_to_wstring
 #include "utility/win32_geometry.h" // get_size
-#include "utility/win32_text.h" // get_window_wtext
+
+LRESULT CALLBACK s_edit_proc
+(HWND hwnd, UINT msg, WPARAM wp, LPARAM lp, UINT_PTR uid, DWORD_PTR data){
+    auto select = reinterpret_cast<Select*>(data);
+    switch(msg) {
+    case WM_PAINT:
+        // NOTE(plu5): The default WM_PAINT seems to trigger the cb WM_PAINT
+        // (in Select::proc). If I suppress it, the cb WM_PAINT never gets
+        // called. However, unless I InvalidateRect beforehand, although the cb
+        // WM_PAINT gets called, its painting doesn't actually apply, it gets
+        // clipped out or something.
+        InvalidateRect(select->hwnd, NULL, false);
+        DefSubclassProc(hwnd, msg, wp, lp);
+        // "An application returns zero if it processes this message."
+        return 0;
+
+    case CB_SETCURSEL: {
+        // NOTE(plu5): CB_SETCURSEL seems to paint over my cb without calling
+        // WM_PAINT. This is a workaround to trigger a repaint of the cb after;
+        // countering its repaint with our own repaint.
+        auto ret = DefSubclassProc(hwnd, msg, wp, lp);
+        InvalidateRect(select->hwnd, NULL, false);
+        // "If the message is successful, the return value is the index of the
+        // item selected."
+        return ret;
+    }
+
+    case WM_NCDESTROY:
+        RemoveWindowSubclass(hwnd, s_edit_proc, uid);
+        return DefSubclassProc(hwnd, msg, wp, lp);
+    }
+    return DefSubclassProc(hwnd, msg, wp, lp);
+}
 
 LRESULT CALLBACK Select::s_proc
 (HWND hwnd, UINT msg, WPARAM wp, LPARAM lp, UINT_PTR uid, DWORD_PTR) {
@@ -19,6 +50,16 @@ LRESULT CALLBACK Select::s_proc
     } else {
         return DefSubclassProc(hwnd, msg, wp, lp);
     }
+}
+
+std::wstring get_cb_text(HWND cb, int i=0) {
+    if (i < 0 or i > ComboBox_GetCount(cb)) return {};
+    auto length_with_terminator = ComboBox_GetLBTextLen(cb, i) + 1;
+    auto buf = new wchar_t[length_with_terminator];
+    ComboBox_GetLBText(cb, i, buf);
+    auto text = std::wstring(buf);
+    delete[] buf;
+    return text;
 }
 
 LRESULT Select::proc
@@ -42,7 +83,7 @@ LRESULT Select::proc
         paint_rect(dc2.h, Theme::edits_bg, &rc);
 
         // text
-        selected_text = get_window_wtext(hwnd);
+        selected_text = get_cb_text(hwnd, ComboBox_GetCurSel(hwnd));
         rc.left += 3;
         if (!selected_text.empty())
             paint_text(dc2.h, selected_text, Theme::fg, &rc, nullptr,
@@ -82,12 +123,16 @@ void Select::initialise
     h = h_;
     hwnd = CreateWindow
         (WC_COMBOBOX, L"",
-         WS_CHILD | WS_VISIBLE | CBS_DROPDOWNLIST,
+         WS_CHILD | WS_VISIBLE | CBS_HASSTRINGS | CBS_DROPDOWNLIST,
          x, y, w, h, parent, NULL, hinst, NULL);
     SetWindowLongPtr(hwnd, GWLP_USERDATA, reinterpret_cast<LPARAM>(this));
     SetWindowSubclass(hwnd, s_proc, static_cast<UINT_PTR>(-1), 0);
-    SetWindowTheme(hwnd, L"DarkMode_Explorer", NULL);
     setup_paint_buffers();
+
+    COMBOBOXINFO info {sizeof(COMBOBOXINFO)};
+    GetComboBoxInfo(hwnd, &info);
+    SetWindowSubclass(info.hwndItem, s_edit_proc, static_cast<UINT_PTR>(-1),
+                      reinterpret_cast<DWORD_PTR>(this));
 }
 
 void Select::add_option(std::string s) {
